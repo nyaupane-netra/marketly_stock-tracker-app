e34aad88b6c81675ab0f9e52d6ec040e450193f9
Netra Nyaupane <nyaupanenetrap@gmail.com>
2026-04-08 16:32:24 -0500

    Implemented search feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = async ({ user }: { user: User }) => {
                 <nav className="hidden sm:block">
                     <NavItems initialStocks={initialStocks} />
                 </nav>
-                <UserDropdown user={user} />
+                <UserDropdown user={user} initialStocks={initialStocks} />
             </div>
         </header>
     )
